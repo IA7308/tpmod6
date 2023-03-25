b5279d36724b186c5e0c3a7089bfb68c1d373bf3
IA7308 <125360024+IA7308@users.noreply.github.com>
2023-03-26 01:42:48 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string tittle;
+    private int playCount;
+
+    public SayaTubeVideo(string tittle)
+    {
+        this.tittle = tittle;
+        this.playCount = 0;
+        Random random = new Random();
+        this.id = random.Next(100000);
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+        this.playCount = this.playCount + playCount;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Judul video " + this.tittle + " Jumlah di putar " + this.playCount + " ID Video " + this.id);
+    }
+
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo Video = new SayaTubeVideo("Tutorial Design By Contract - [Iqnaz_Nadhif]");
+        Video.PrintVideoDetails();
+        Video.IncreasePlayCount(1);
+        Video.PrintVideoDetails();
+        Video.IncreasePlayCount(2);
+        Video.PrintVideoDetails();
+    }
+}
